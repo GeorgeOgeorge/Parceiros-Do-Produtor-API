24f76f0e5beeda37c62273c69cbd42960f456b21
George Mendonça <61426720+GeorgeOgeorge@users.noreply.github.com>
2022-03-19 18:13:05 -0300

    admin created and serialzier modivied

--- api_v1/admin.py
+++ api_v1/admin.py
@@ -1,3 +1,61 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Farmer, Propertie, Animal, Milking
+
+
+@admin.register(Farmer)
+class FarmerAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'age',
+        'phone',
+        'sex',
+        'active',
+        'created',
+        'updated'
+        'properties'
+    ]
+
+
+@admin.register(Propertie)
+class PropertieAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'phone',
+        'longitude',
+        'latitude',
+        'active',
+        'created',
+        'updated'
+        'animals'
+    ]
+
+
+@admin.register(Animal)
+class AnimalAdmin(admin.ModelAdmin):
+    list_display = [
+        'name',
+        'sex',
+        'breed',
+        'code',
+        'furr_color'
+        'purchased',
+        'birth_date',
+        'active',
+        'created',
+        'updated'
+        'milkings'
+    ]
+
+
+@admin.register(Milking)
+class MilkingAdmin(admin.ModelAdmin):
+    list_display = [
+        'value',
+        'date',
+        'shift',
+        'dry'
+        'active',
+        'created',
+        'updated'
+    ]
--- api_v1/views.py
+++ api_v1/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Animal, Farmer, Milking, Propertie
+from .serializers import AnimalSerializer, FarmerSerializer, MilkingSerializer, PropertieSerializer
+
+# TODO: create path to list relations  
+
+class FarmerViewSet(viewsets.ModelViewSet):
+    queryset = Farmer.objects.all()
+    serializer_class = FarmerSerializer
+
+class PropertieViewSet(viewsets.ModelViewSet):
+    queryset = Propertie.objects.all()
+    serializer_class = PropertieSerializer
+
+class AnimalViewSet(viewsets.ModelViewSet):
+    queryset = Animal.objects.all()
+    serializer_class = AnimalSerializer
+
+class MilkingViewSet(viewsets.ModelViewSet):
+    queryset = Milking.objects.all()
+    serializer_class = MilkingSerializer
