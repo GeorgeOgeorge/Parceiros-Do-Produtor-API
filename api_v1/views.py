e015aeda94760eb8d7d4d0d872a49aa0bc4c3cfe
George Mendonça <61426720+GeorgeOgeorge@users.noreply.github.com>
2022-03-23 20:53:40 -0300

    removed extra actions

--- api_v1/views.py
+++ api_v1/views.py
@@ -1,30 +1,26 @@
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+from rest_framework import status
 
 from .models import Animal, Farmer, Milking, Propertie
 from .serializers import AnimalSerializer, FarmerSerializer, MilkingSerializer, PropertieSerializer
 
-# TODO: create path to list relations  
 
 class FarmerViewSet(viewsets.ModelViewSet):
     queryset = Farmer.objects.all()
     serializer_class = FarmerSerializer
-
-    @action(detail=True, methods=['get'])
-    def properties(self, request, pk=None):
-        properties = Propertie.objects.filter(id=pk)
-        serializer = PropertieSerializer(properties.all(), many=True)
-        return Response(serializer.data)
 
 
 class PropertieViewSet(viewsets.ModelViewSet):
     queryset = Propertie.objects.all()
     serializer_class = PropertieSerializer
 
+
 class AnimalViewSet(viewsets.ModelViewSet):
     queryset = Animal.objects.all()
     serializer_class = AnimalSerializer
+
 
 class MilkingViewSet(viewsets.ModelViewSet):
     queryset = Milking.objects.all()
